e835056ddb712b2dcbcf8cb30b896dd32c8c9490
ThaqibK <149933406+ThaqibK@users.noreply.github.com>
2025-04-22 16:12:35 +0530

    First commit

---
+++ float.py
@@ -0,0 +1,14 @@
+x = float(input("Enter the value of x = "))
+y = float(input("Enter the value of y = "))
+
+z = (x*y)
+
+a = round (x*y) #This round of to the closest integer
+
+print(z)
+print(a)
+
+
+print(f"{z:,}") # This adds (,) in numerical system so we can easily number it
+
+print(f"{z:.2f}") # This .2f round it off to 2 decimal places in numerical system so we can easily number it
---
+++ int.py
@@ -0,0 +1,19 @@
+x = int(input("Enter the value of x = "))
+y = int(input("Enter the value of y = "))
+
+print(x*y)
+
+'''
+--- or --- 
+
+x = input("Enter the value of x = ")
+y = input("Enter the value of y = ")
+
+z = int(x)+ int(y)
+
+print(z)
+
+--- or ---
+
+print((int(input("Enter the value of x = "))) + (int(input("Enter the value of y = "))))
+'''
